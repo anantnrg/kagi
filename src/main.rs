1fd7026d89bdbe0e769cc547e6d8e4e75151d730
Anant Narayan <anantnrg@gmail.com>
2024-12-26 15:32:27 +0530

    Freya integration in progress

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use anyhow::Error;
 use glib::MainLoop;
 use gstreamer::prelude::*;
+use freya::prelude::*;
 
 fn main() -> Result<(), Error> {
     gstreamer::init()?;
 
-    let uri = "file:///assets/music.mp3";
+    let uri = "file:///D:/repos/reyvr/assets/music.mp3";
 
     let playbin = gstreamer::ElementFactory::make("playbin")
         .name("playbin")
@@ -19,4 +20,28 @@ fn main() -> Result<(), Error> {
 
     playbin.set_state(gstreamer::State::Null)?;
     Ok(())
+}
+
+fn app() -> Element {
+    rsx!(
+        Body {
+            rect {
+                width: "fill",
+                height: "fill",
+                main_align: "center",
+                cross_align: "center",
+                Button {
+                    onclick: move |_| println!("Button Clicked!"),
+                    label { "Button A" }
+                }
+                FilledButton {
+                    onpress: move |_| println!("Button Pressed!"),
+                    label { "Button B" }
+                }
+                OutlineButton {
+                    label { "Button C" }
+                }
+            }
+        }
+    )
 }
