1ee71335fe077d4360e1727b577b06ccb52806d3
Anant Narayan <anantnrg@gmail.com>
2024-12-26 14:00:31 +0530

    remove unneecssary comments

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,22 @@
 use anyhow::Error;
 use glib::MainLoop;
-use gstreamer::{ElementFactory, Pipeline, State, prelude::*};
+use gstreamer::prelude::*;
 
 fn main() -> Result<(), Error> {
-    // Initialize gstreamerreamer
     gstreamer::init()?;
 
-    let uri = "file:///assets/music.mp3"; // File located in the root directory of the repo
+    let uri = "file:///assets/music.mp3";
 
-    // Create PlayBin element
     let playbin = gstreamer::ElementFactory::make("playbin")
         .name("playbin")
-        // Set URI to play
         .property("uri", uri)
         .build()?;
 
-    // Set the playbin to PLAYING
     playbin.set_state(gstreamer::State::Playing)?;
 
-    // Set up the main loop
     let main_loop = MainLoop::new(None, false);
     main_loop.run();
 
-    // Cleanup
     playbin.set_state(gstreamer::State::Null)?;
-
     Ok(())
 }
