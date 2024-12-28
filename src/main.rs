a67b41ffa0ce52ea9822bc3fa775a3abda1aa844
Anant Narayan <anantnrg@gmail.com>
2024-12-28 22:13:28 +0530

    Switch to GPUI

--- src/main.rs
+++ src/main.rs
@@ -1,182 +1,45 @@
 use gpui::{
-    App, AppContext, Bounds, SharedString, Timer, ViewContext, WindowBounds, WindowContext,
-    WindowKind, WindowOptions, div, prelude::*, px, rgb, size,
+    div, prelude::*, px, rgb, size, App, AppContext, Bounds, SharedString, TitlebarOptions, ViewContext, WindowBounds, WindowOptions
 };
 
-struct SubWindow {
-    custom_titlebar: bool,
+struct Reyvr {
+    title: SharedString
 }
 
-fn button(text: &str, on_click: impl Fn(&mut WindowContext) + 'static) -> impl IntoElement {
-    div()
-        .id(SharedString::from(text.to_string()))
-        .flex_none()
-        .px_2()
-        .bg(rgb(0xf7f7f7))
-        .active(|this| this.opacity(0.85))
-        .border_1()
-        .border_color(rgb(0xe0e0e0))
-        .rounded_md()
-        .cursor_pointer()
-        .child(text.to_string())
-        .on_click(move |_, cx| on_click(cx))
-}
-
-impl Render for SubWindow {
-    fn render(&mut self, _: &mut ViewContext<Self>) -> impl IntoElement {
+impl Render for Reyvr {
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .flex()
-            .flex_col()
-            .bg(rgb(0xffffff))
-            .size_full()
-            .gap_2()
-            .when(self.custom_titlebar, |cx| {
-                cx.child(
-                    div()
-                        .flex()
-                        .h(px(32.))
-                        .px_4()
-                        .bg(gpui::blue())
-                        .text_color(gpui::white())
-                        .w_full()
-                        .child(
-                            div()
-                                .flex()
-                                .items_center()
-                                .justify_center()
-                                .size_full()
-                                .child("Custom Titlebar"),
-                        ),
-                )
-            })
-            .child(
-                div()
-                    .p_8()
-                    .gap_2()
-                    .child("SubWindow")
-                    .child(button("Close", |cx| {
-                        cx.remove_window();
-                    })),
-            )
-    }
-}
-
-struct WindowDemo {}
-
-impl Render for WindowDemo {
-    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
-        let window_bounds =
-            WindowBounds::Windowed(Bounds::centered(None, size(px(300.0), px(300.0)), cx));
-
-        div()
-            .p_4()
-            .flex()
-            .flex_wrap()
-            .bg(rgb(0xffffff))
-            .size_full()
+            .bg(rgb(0x1e1e2d))
+            .size(px(500.0))
             .justify_center()
             .items_center()
-            .gap_2()
-            .child(button("Normal", move |cx| {
-                cx.open_window(
-                    WindowOptions {
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |cx| {
-                        cx.new_view(|_cx| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Popup", move |cx| {
-                cx.open_window(
-                    WindowOptions {
-                        window_bounds: Some(window_bounds),
-                        kind: WindowKind::PopUp,
-                        ..Default::default()
-                    },
-                    |cx| {
-                        cx.new_view(|_cx| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Custom Titlebar", move |cx| {
-                cx.open_window(
-                    WindowOptions {
-                        titlebar: None,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |cx| {
-                        cx.new_view(|_cx| SubWindow {
-                            custom_titlebar: true,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Invisible", move |cx| {
-                cx.open_window(
-                    WindowOptions {
-                        show: false,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |cx| {
-                        cx.new_view(|_cx| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Unmovable", move |cx| {
-                cx.open_window(
-                    WindowOptions {
-                        is_movable: false,
-                        titlebar: None,
-                        window_bounds: Some(window_bounds),
-                        ..Default::default()
-                    },
-                    |cx| {
-                        cx.new_view(|_cx| SubWindow {
-                            custom_titlebar: false,
-                        })
-                    },
-                )
-                .unwrap();
-            }))
-            .child(button("Hide Application", |cx| {
-                cx.hide();
-
-                // Restore the application after 3 seconds
-                cx.spawn(|mut cx| async move {
-                    Timer::after(std::time::Duration::from_secs(3)).await;
-                    cx.update(|cx| {
-                        cx.activate(false);
-                    })
-                })
-                .detach();
-            }))
+            .text_color(rgb(0xffffff))
+            .child(format!("{}", &self.title))
     }
 }
 
 fn main() {
     App::new().run(|cx: &mut AppContext| {
-        let bounds = Bounds::centered(None, size(px(800.0), px(600.0)), cx);
-        cx.open_window(
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                ..Default::default()
-            },
-            |cx| cx.new_view(|_cx| WindowDemo {}),
-        )
-        .unwrap();
-    });
-}
+            let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+            cx.open_window(
+                WindowOptions {
+                    window_bounds: Some(WindowBounds::Windowed(bounds)),
+                    app_id: Some(String::from("reyvr")),
+                    focus: true,
+                    titlebar: Some(TitlebarOptions {
+                        title: Some(SharedString::new_static("Reyvr")),
+                        appears_transparent: false,
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                },
+                |cx| {
+                    cx.new_view(|_cx| Reyvr {
+                        title: "Reyvr".into(),
+                    })
+                },
+            )
+            .unwrap();
+        });
+    }
