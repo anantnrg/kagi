735e4d99bff174d6644a0a9a8c412fafa14c24f0
Anant Narayan <anantnrg@gmail.com>
2025-01-02 22:47:04 +0530

    More work on slider

--- src/components/slider.rs
+++ src/components/slider.rs
@@ -1,4 +1,4 @@
-use gpui::{Bounds, EntityId, Pixels, Render};
+use gpui::{Bounds, EntityId, Pixels, Point, Render};
 
 #[derive(Clone, Copy, Render)]
 pub struct Thumb(EntityId);
@@ -15,13 +15,14 @@ pub struct Slider {
     bounds: Bounds<Pixels>,
 }
 
+#[allow(dead_code)]
 impl Slider {
     fn new() -> Self {
         Slider {
             min: 0.0,
             max: 100.0,
             step: 1.0,
-            value: 0.0,
+            value: 100.0,
             bounds: Bounds::default(),
         }
     }
@@ -34,5 +35,31 @@ impl Slider {
     fn max(mut self, value: f32) -> Self {
         self.max = value;
         self
+    }
+
+    fn step(mut self, value: f32) -> Self {
+        self.step = value;
+        self
+    }
+
+    fn default(mut self) -> Self {
+        self.value = 0.0;
+        self
+    }
+
+    fn set(mut self, value: f32) -> Self {
+        self.value = value;
+        self
+    }
+
+    fn handle_drag(&mut self, position: Point<Pixels>, cx: &mut gpui::ViewContext<Self>) {
+        let bounds = self.bounds;
+        let min = self.min;
+        let max = self.max;
+        let step = self.step;
+
+        let relative = (position.x - bounds.left()) / bounds.size.width;
+        let value = ((min + (max - min) * relative) / step).round() * step;
+        self.value = value.clamp(self.min, self.max);
     }
 }
