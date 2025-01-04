994018e250db359b122915594b3cdb8883e8dde7
Anant Narayan <anantnrg@gmail.com>
2025-01-04 21:38:19 +0530

    Test out small example

--- crates/reyvr/src/main.rs
+++ crates/reyvr/src/main.rs
@@ -1,5 +1,46 @@
+use components::button::Button;
 use gpui::*;
 
+struct Reyvr {
+    title: SharedString,
+}
+
+impl Render for Reyvr {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        cx.set_window_title(self.title.to_string().as_str());
+        div()
+            .flex()
+            .gap_8()
+            .bg(rgb(0x1e1e2d))
+            .size_full()
+            .justify_center()
+            .items_center()
+            .text_color(rgb(0xffffff))
+            .child(Button::new("button").on_click(|_, _| println!("Clciked")))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new().run(|cx: &mut AppContext| {
+        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                app_id: Some(String::from("reyvr")),
+                focus: true,
+                titlebar: Some(TitlebarOptions {
+                    title: Some(SharedString::new_static("Reyvr")),
+                    appears_transparent: false,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            |cx| {
+                cx.new_view(|_cx| Reyvr {
+                    title: "Reyvr - Nothing playing.".into(),
+                })
+            },
+        )
+        .unwrap();
+    });
 }
