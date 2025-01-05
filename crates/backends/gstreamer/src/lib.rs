ad989fb639641d53c7f0f36e4cc3482d2fee8d1f
Anant Narayan <anantnrg@gmail.com>
2025-01-05 21:56:13 +0530

    Start implementing Backend trait for GstBackend

--- crates/backends/gstreamer/src/lib.rs
+++ crates/backends/gstreamer/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::anyhow;
+use backend::Backend;
+use gstreamer::prelude::*;
+use std::sync::{Arc, Mutex};
+
+pub struct GstBackend {
+    pub playbin: Arc<Mutex<gstreamer::Element>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Backend for GstBackend {
+    fn init() -> anyhow::Result<()> {
+        gstreamer::init().map_err(|e| anyhow!("Failed to initialize Gstreamer backend: {e}"))
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn load(&self, uri: &str) -> anyhow::Result<()> {
+        let playbin = Arc::clone(&self.playbin);
+        playbin
+            .lock()
+            .expect("Could not lock playbin")
+            .set_property("uri", uri);
+        Ok(())
+    }
+
+    fn play(&self) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    fn pause(&self) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    fn stop(&self) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    fn set_volume(&self, volume: f32) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    fn get_volume(&self) -> anyhow::Result<f32> {
+        todo!()
+    }
+
+    fn get_state(&self) -> anyhow::Result<backend::PlaybackState> {
+        todo!()
     }
 }
