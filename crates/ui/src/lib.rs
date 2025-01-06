f477bbd3c7c002bee6cae48e976e709e3525b06e
Anant Narayan <anantnrg@gmail.com>
2025-01-06 09:36:52 +0530

    Work on render

--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod app;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use anyhow::anyhow;
+use app::Reyvr;
+use gpui::*;
+use std::sync::{Arc, Mutex};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn run_app(playbin: Arc<Mutex<gstreamer::Element>>) -> anyhow::Result<()> {
+    let app = App::new();
+
+    app.run(|cx: &mut AppContext| {
+        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+        cx.open_window(
+            WindowOptions {
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                app_id: Some(String::from("reyvr")),
+                focus: true,
+                titlebar: Some(TitlebarOptions {
+                    title: Some(SharedString::new_static("Reyvr")),
+                    appears_transparent: false,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            |cx| {
+                cx.new_view(|_cx| Reyvr {
+                    title: "Reyvr - Nothing playing.".into(),
+                    playbin,
+                    volume: Arc::new(Mutex::new(0.5)),
+                })
+            },
+        )
+        .map_err(|e| anyhow!("Could not run app: {e}"));
+    });
+    Ok(())
 }
