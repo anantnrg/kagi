41bd2216eff2c58608498b04e7c655e4c5f0d274
Anant Narayan <anantnrg@gmail.com>
2025-01-06 14:15:43 +0530

    Create backend and run app

--- crates/reyvr/src/main.rs
+++ crates/reyvr/src/main.rs
@@ -1,9 +1,12 @@
 use anyhow::Error;
+use backend::Backend as _;
 use gst_backend::GstBackend;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use ui::run_app;
 
 fn main() -> Result<(), Error> {
-    GstBackend::init();
+    GstBackend::init().expect("Could not initialize GStreamer backend: {e}");
+    let backend = GstBackend::new().expect("Could not create GStreamer backend");
+    run_app(Arc::new(backend)).expect("Could not run app");
     Ok(())
 }
