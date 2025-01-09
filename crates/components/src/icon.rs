99b2fb7b5391b5425ae759d2510b5d82ad77e7b1
Anant Narayan <anantnrg@gmail.com>
2025-01-09 21:11:39 +0530

    Create Icons enum

--- crates/components/src/icon.rs
+++ crates/components/src/icon.rs
@@ -1,1 +1,25 @@
 use gpui::*;
+
+pub enum Icons {
+    Close,
+    Maximize,
+    Minimize,
+    Restore,
+}
+
+impl Icons {
+    pub fn path(self) -> SharedString {
+        match self {
+            Self::Close => "",
+            Self::Restore => "",
+            Self::Maximize => "",
+            Self::Minimize => "",
+        }
+        .into()
+    }
+}
+
+pub struct Icon {
+    pub icon: Icons,
+    pub size: Pixels,
+}
