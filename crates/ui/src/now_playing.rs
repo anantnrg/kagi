6ff9741bad548cfef45dd1a50e823c253c2a263b
Anant Narayan <anantnrg@gmail.com>
2025-01-10 20:49:53 +0530

    Implement NowPlaying

--- crates/ui/src/now_playing.rs
+++ crates/ui/src/now_playing.rs
@@ -5,3 +5,28 @@ pub struct NowPlaying {
     pub artists: Vec<SharedString>,
     pub album: SharedString,
 }
+
+pub enum NowPlayingEvent {
+    Title(SharedString),
+    Artists(Vec<SharedString>),
+    Album(SharedString),
+}
+
+impl NowPlaying {
+    pub fn new() -> Self {
+        NowPlaying {
+            title: "".into(),
+            artists: vec!["".into()],
+            album: "".into(),
+        }
+    }
+
+    pub fn change_title(mut self, cx: &mut ViewContext<Self>, title: SharedString) -> Self {
+        self.title = title.clone();
+        cx.emit(NowPlayingEvent::Title(title));
+        cx.notify();
+        self
+    }
+}
+
+impl EventEmitter<NowPlayingEvent> for NowPlaying {}
