f13e36f91acae0cb35acb001270ee6474e163306
Anant Narayan <anantnrg@gmail.com>
2025-01-12 14:06:01 +0530

    Start working on playlist

--- crates/backend/src/playback.rs
+++ crates/backend/src/playback.rs
@@ -1,3 +1,5 @@
+use crate::Backend;
+
 #[derive(Debug, Clone)]
 pub struct Track {
     pub title: String,
@@ -13,4 +15,29 @@ pub struct Playlist {
     pub name: String,
     pub tracks: Vec<Track>,
     pub current_index: usize,
+}
+
+impl Playlist {
+    pub fn load_current_song<B: Backend>(&self, backend: &B) -> anyhow::Result<()> {
+        let current_song = &self.tracks[self.current_index];
+        backend.load(&current_song.uri)?;
+        backend.play()?;
+        Ok(())
+    }
+
+    pub fn play_next<B: Backend>(&mut self, backend: &B) -> anyhow::Result<()> {
+        if self.current_index + 1 < self.tracks.len() {
+            self.current_index += 1;
+            self.load_current_song(backend)?;
+        }
+        Ok(())
+    }
+
+    pub fn play_previous<B: Backend>(&mut self, backend: &B) -> anyhow::Result<()> {
+        if self.current_index > 0 {
+            self.current_index -= 1;
+            self.load_current_song(backend)?;
+        }
+        Ok(())
+    }
 }
