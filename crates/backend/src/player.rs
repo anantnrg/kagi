9e8521091ee5303ba2d1bb7e15534e1d7c2bc9df
Anant Narayan <anantnrg@gmail.com>
2025-01-21 16:03:59 +0530

    Add enums

--- crates/backend/src/player.rs
+++ crates/backend/src/player.rs
@@ -2,12 +2,33 @@ use std::sync::{Arc, Mutex};
 
 use smol::channel::{Receiver, Sender};
 
-use crate::{Backend, playback::Playlist};
+use crate::{
+    Backend,
+    playback::{Playlist, Track},
+};
+
+pub enum Command {
+    Play,
+    Pause,
+    Volume(f64),
+    GetMeta,
+}
+
+pub enum Response {
+    Error(String),
+    Success(String),
+    Metadata(Track),
+}
 
 pub struct Player {
     pub backend: Arc<Box<dyn Backend>>,
     pub playlist: Arc<Mutex<Playlist>>,
     pub volume: f64,
-    pub tx: Sender<String>,
-    pub rx: Receiver<String>,
+    pub tx: Sender<Response>,
+    pub rx: Receiver<Command>,
+}
+
+pub struct Controller {
+    pub tx: Sender<Command>,
+    pub rx: Receiver<Response>,
 }
