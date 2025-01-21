ca60e31fe97387d4c1545eea97ee1dff79a1e81f
Anant Narayan <anantnrg@gmail.com>
2025-01-21 11:57:41 +0530

    Start working on Player struct

--- crates/backend/src/player.rs
+++ crates/backend/src/player.rs
@@ -1,1 +1,13 @@
-pub struct Player {}
+use std::sync::{Arc, Mutex};
+
+use smol::channel::{Receiver, Sender};
+
+use crate::{Backend, playback::Playlist};
+
+pub struct Player {
+    pub backend: Arc<Box<dyn Backend>>,
+    pub playlist: Arc<Mutex<Playlist>>,
+    pub volume: f64,
+    pub tx: Sender<String>,
+    pub rx: Receiver<String>,
+}
