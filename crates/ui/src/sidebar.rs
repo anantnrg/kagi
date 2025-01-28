f44364e6965d472ccbf00bfb28275c6b7d474ae2
Anant Narayan <anantnrg@gmail.com>
2025-01-28 08:42:46 +0530

    Tweaks

--- crates/ui/src/sidebar.rs
+++ crates/ui/src/sidebar.rs
@@ -10,23 +10,26 @@ impl Render for LeftSidebar {
     fn render(&mut self, win: &mut Window, cx: &mut Context<'_, Self>) -> impl IntoElement {
         let window_width = win.window_bounds().get_bounds().size.width.0;
         let theme = cx.global::<Theme>();
-        if window_width < 200.0 {
+        if window_width < 400.0 {
             div()
         } else {
             div()
                 .bg(theme.background)
                 .h_full()
-                .w(px(window_width * (20.0 / 100.0)))
-                .min_w(px(150.0))
+                .w(px(window_width * (30.0 / 100.0)))
+                .min_w(px(240.0))
                 .border_r_1()
                 .border_color(theme.secondary)
                 .children(self.playlists.read(cx).into_iter().map(|(name, _)| {
                     div()
+                        .bg(theme.secondary)
                         .w_full()
-                        .mx_4()
+                        .mx_2()
                         .rounded_lg()
                         .h_10()
                         .flex()
+                        .items_start()
+                        .px_2()
                         .child(name.clone())
                 }))
         }
