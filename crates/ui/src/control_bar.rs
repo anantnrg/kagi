379e2896d79ec217ceb5db844a68f61d447ac5d2
Anant Narayan <anantnrg@gmail.com>
2025-02-10 21:03:20 +0530

    Start working on playbar

--- crates/ui/src/control_bar.rs
+++ crates/ui/src/control_bar.rs
@@ -13,6 +13,7 @@ use crate::now_playing::NowPlaying;
 pub struct ControlBar {
     now_playing: Entity<NowPlaying>,
     vol_slider: Entity<Slider>,
+    playbar: Entity<Slider>,
 }
 
 impl Render for ControlBar {
@@ -32,108 +33,130 @@ impl Render for ControlBar {
             .justify_center()
             .justify_between()
             .px_4()
-            .child(div().w_full().h_full())
             .child(
                 div()
-                    .flex()
-                    .h_full()
                     .w_full()
+                    .id("playbar")
+                    .h_full()
+                    .flex()
                     .items_center()
                     .justify_center()
-                    .overflow_hidden()
-                    .gap_3()
-                    .child(
-                        div()
-                            .size_6()
-                            .flex()
-                            .items_center()
-                            .justify_center()
-                            .child(
-                                Icon::new(Icons::Previous)
-                                    .size(32.0)
-                                    .color(theme.text.into()),
-                            )
-                            .on_mouse_down(MouseButton::Left, {
-                                {
-                                    let controller = controller.clone();
-                                    move |_, _, _| {
-                                        controller.prev();
-                                    }
-                                }
-                            }),
-                    )
-                    .child(
-                        div()
-                            .size_6()
-                            .flex()
-                            .items_center()
-                            .justify_center()
-                            .when(np.state == State::Null, |this| {
-                                this.child(
-                                    Icon::new(Icons::Stopped)
-                                        .size(32.0)
-                                        .color(theme.text.into()),
-                                )
-                            })
-                            .when(np.state == State::Playing, |this| {
-                                this.child(
-                                    Icon::new(Icons::Pause).size(32.0).color(theme.text.into()),
-                                )
-                            })
-                            .when(np.state == State::Paused, |this| {
-                                this.child(
-                                    Icon::new(Icons::Play).size(32.0).color(theme.text.into()),
-                                )
-                            })
-                            .on_mouse_down(MouseButton::Left, {
-                                {
-                                    let controller = controller.clone();
-                                    let np = np.clone();
-                                    move |_, _, _| {
-                                        if np.state == State::Playing {
-                                            controller.pause();
-                                        } else if np.state == State::Null
-                                            || np.state == State::Paused
-                                        {
-                                            controller.play();
-                                        }
-                                    }
-                                }
-                            }),
-                    )
-                    .child(
-                        div()
-                            .size_6()
-                            .flex()
-                            .items_center()
-                            .justify_center()
-                            .child(Icon::new(Icons::Next).size(32.0).color(theme.text.into()))
-                            .on_mouse_down(MouseButton::Left, {
-                                {
-                                    let controller = controller.clone();
-                                    move |_, _, _| {
-                                        controller.next();
-                                    }
-                                }
-                            }),
-                    ),
+                    .px_8()
+                    .child(self.playbar.clone()),
             )
             .child(
                 div()
-                    .flex()
-                    .items_center()
-                    .justify_end()
-                    .h_auto()
                     .w_full()
-                    .px_2()
-                    .child(div().w_20().child(self.vol_slider.clone()))
+                    .h_8()
+                    .child(div().w_full().h_full())
                     .child(
                         div()
-                            .child(format!("{:.0}%", np.volume * 100.0))
-                            .text_color(theme.text)
-                            .ml_4()
-                            .w_10()
-                            .overflow_hidden(),
+                            .flex()
+                            .h_full()
+                            .w_full()
+                            .items_center()
+                            .justify_center()
+                            .overflow_hidden()
+                            .gap_3()
+                            .child(
+                                div()
+                                    .size_6()
+                                    .flex()
+                                    .items_center()
+                                    .justify_center()
+                                    .child(
+                                        Icon::new(Icons::Previous)
+                                            .size(32.0)
+                                            .color(theme.text.into()),
+                                    )
+                                    .on_mouse_down(MouseButton::Left, {
+                                        {
+                                            let controller = controller.clone();
+                                            move |_, _, _| {
+                                                controller.prev();
+                                            }
+                                        }
+                                    }),
+                            )
+                            .child(
+                                div()
+                                    .size_6()
+                                    .flex()
+                                    .items_center()
+                                    .justify_center()
+                                    .when(np.state == State::Null, |this| {
+                                        this.child(
+                                            Icon::new(Icons::Stopped)
+                                                .size(32.0)
+                                                .color(theme.text.into()),
+                                        )
+                                    })
+                                    .when(np.state == State::Playing, |this| {
+                                        this.child(
+                                            Icon::new(Icons::Pause)
+                                                .size(32.0)
+                                                .color(theme.text.into()),
+                                        )
+                                    })
+                                    .when(np.state == State::Paused, |this| {
+                                        this.child(
+                                            Icon::new(Icons::Play)
+                                                .size(32.0)
+                                                .color(theme.text.into()),
+                                        )
+                                    })
+                                    .on_mouse_down(MouseButton::Left, {
+                                        {
+                                            let controller = controller.clone();
+                                            let np = np.clone();
+                                            move |_, _, _| {
+                                                if np.state == State::Playing {
+                                                    controller.pause();
+                                                } else if np.state == State::Null
+                                                    || np.state == State::Paused
+                                                {
+                                                    controller.play();
+                                                }
+                                            }
+                                        }
+                                    }),
+                            )
+                            .child(
+                                div()
+                                    .size_6()
+                                    .flex()
+                                    .items_center()
+                                    .justify_center()
+                                    .child(
+                                        Icon::new(Icons::Next).size(32.0).color(theme.text.into()),
+                                    )
+                                    .on_mouse_down(MouseButton::Left, {
+                                        {
+                                            let controller = controller.clone();
+                                            move |_, _, _| {
+                                                controller.next();
+                                            }
+                                        }
+                                    }),
+                            ),
+                    )
+                    .child(
+                        div()
+                            .flex()
+                            .items_center()
+                            .justify_end()
+                            .h_auto()
+                            .w_full()
+                            .px_2()
+                            .child(div().w_20().child(self.vol_slider.clone()))
+                            .child(
+                                div()
+                                    .child(format!("{:.0}%", np.volume * 100.0))
+                                    .text_color(theme.text)
+                                    .ml_4()
+                                    .w_10()
+                                    .overflow_hidden(),
+                            ),
                     ),
             )
     }
