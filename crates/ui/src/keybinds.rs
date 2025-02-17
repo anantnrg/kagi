501fe61f59530e68a4dcc5a877cbb5cd40d76259
Anant Narayan <anantnrg@gmail.com>
2025-02-17 20:28:55 +0530

    volume controls, but -0 issue

--- crates/ui/src/keybinds.rs
+++ crates/ui/src/keybinds.rs
@@ -3,15 +3,19 @@ use backend::player::Controller;
 use gpui::*;
 use gstreamer::State;
 
-actions!(kagi, [ChangeState, Prev, Next]);
+actions!(kagi, [ChangeState, Prev, Next, VolUp, VolDown]);
 
 pub fn register(cx: &mut App) {
     cx.on_action(change_state);
     cx.on_action(next);
     cx.on_action(previous);
+    cx.on_action(vol_up);
+    cx.on_action(vol_down);
     cx.bind_keys([KeyBinding::new("space", ChangeState, None)]);
     cx.bind_keys([KeyBinding::new("ctrl-left", Prev, None)]);
     cx.bind_keys([KeyBinding::new("ctrl-right", Next, None)]);
+    cx.bind_keys([KeyBinding::new("up", VolUp, None)]);
+    cx.bind_keys([KeyBinding::new("down", VolDown, None)]);
 }
 
 pub fn change_state(_: &ChangeState, cx: &mut App) {
@@ -30,4 +34,42 @@ fn next(_: &Next, cx: &mut App) {
 
 fn previous(_: &Prev, cx: &mut App) {
     cx.global::<Controller>().prev();
+}
+
+fn vol_up(_: &VolUp, cx: &mut App) {
+    let state = cx.global_mut::<PlayerContext>().state.clone();
+
+    let curr_vol = cx.global::<PlayerContext>().state.read(cx).volume;
+    let mut new_vol: f64;
+
+    if curr_vol < 1.0 {
+        new_vol = curr_vol + 0.05;
+    } else {
+        new_vol = curr_vol;
+    }
+
+    state.update(cx, |this, cx| {
+        this.volume = new_vol.clone();
+        cx.notify();
+    });
+    cx.global::<Controller>().volume(new_vol);
+}
+
+fn vol_down(_: &VolDown, cx: &mut App) {
+    let state = cx.global_mut::<PlayerContext>().state.clone();
+
+    let curr_vol = cx.global::<PlayerContext>().state.read(cx).volume;
+    let new_vol: f64;
+
+    if curr_vol > 0.0 {
+        new_vol = curr_vol - 0.05;
+    } else {
+        new_vol = curr_vol;
+    }
+
+    state.update(cx, |this, cx| {
+        this.volume = new_vol.clone();
+        cx.notify();
+    });
+    cx.global::<Controller>().volume(new_vol);
 }
