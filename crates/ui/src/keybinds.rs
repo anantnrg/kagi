8ac735372377598a6867c8ecf90f778f65243fab
Anant Narayan <anantnrg@gmail.com>
2025-02-17 21:46:28 +0530

    Seeking

--- crates/ui/src/keybinds.rs
+++ crates/ui/src/keybinds.rs
@@ -3,7 +3,15 @@ use backend::player::Controller;
 use gpui::*;
 use gstreamer::State;
 
-actions!(kagi, [ChangeState, Prev, Next, VolUp, VolDown]);
+actions!(kagi, [
+    ChangeState,
+    Prev,
+    Next,
+    VolUp,
+    VolDown,
+    SeekForward,
+    SeekBackward
+]);
 
 pub fn register(cx: &mut App) {
     cx.on_action(change_state);
@@ -11,9 +19,13 @@ pub fn register(cx: &mut App) {
     cx.on_action(previous);
     cx.on_action(vol_up);
     cx.on_action(vol_down);
+    cx.on_action(seek_forward);
+    cx.on_action(seek_backward);
     cx.bind_keys([KeyBinding::new("space", ChangeState, None)]);
     cx.bind_keys([KeyBinding::new("ctrl-left", Prev, None)]);
     cx.bind_keys([KeyBinding::new("ctrl-right", Next, None)]);
+    cx.bind_keys([KeyBinding::new("left", SeekBackward, None)]);
+    cx.bind_keys([KeyBinding::new("right", SeekForward, None)]);
     cx.bind_keys([KeyBinding::new("up", VolUp, None)]);
     cx.bind_keys([KeyBinding::new("down", VolDown, None)]);
 }
@@ -62,4 +74,14 @@ fn vol_down(_: &VolDown, cx: &mut App) {
         cx.notify();
     });
     cx.global::<Controller>().volume(new_vol);
+}
+
+fn seek_forward(_: &SeekForward, cx: &mut App) {
+    let current_pos = cx.global::<PlayerContext>().state.read(cx).position;
+    cx.global::<Controller>().seek(current_pos + 5);
+}
+
+fn seek_backward(_: &SeekBackward, cx: &mut App) {
+    let current_pos = cx.global::<PlayerContext>().state.read(cx).position;
+    cx.global::<Controller>().seek(current_pos - 5);
 }
