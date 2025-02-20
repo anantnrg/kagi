ee9d837c95ed2044d25097a940b16bc016845b3d
Anant Narayan <anantnrg@gmail.com>
2025-02-20 22:13:16 +0530

    impl functions for Theme

--- crates/backend/src/theme.rs
+++ crates/backend/src/theme.rs
@@ -1,3 +1,6 @@
+use std::{fs, path::PathBuf};
+
+use directories::UserDirs;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Copy, Serialize, Deserialize)]
@@ -11,4 +14,58 @@ pub struct Theme {
     pub main_bg: u32,
     pub titlebar_bg: u32,
     pub highlight: u32,
+}
+
+impl Theme {
+    pub fn default() -> Self {
+        Theme {
+            accent: 0xcba6f7,
+            text: 0xcdd6f4,
+            icon: 0xcdd6f4,
+            background: 0x11111B,
+            secondary: 0x1e1e2d,
+            sidebar_bg: 0x11111B,
+            main_bg: 0x11111B,
+            titlebar_bg: 0x11111B,
+            highlight: 0x52cba6f7,
+        }
+    }
+    pub fn get_file() -> Option<PathBuf> {
+        if let Some(user_dirs) = UserDirs::new() {
+            let proj_dir = user_dirs
+                .audio_dir()
+                .unwrap_or(user_dirs.home_dir())
+                .join("Kagi");
+            if let Err(e) = fs::create_dir_all(proj_dir.clone()) {
+                eprintln!("Could not create config directory: {}", e);
+                return None;
+            }
+            Some(proj_dir.join("theme.toml"))
+        } else {
+            None
+        }
+    }
+    pub fn load() -> Self {
+        if let Some(file_path) = Self::get_file() {
+            if file_path.exists() {
+                match fs::read_to_string(&file_path) {
+                    Ok(contents) => match toml::from_str(&contents) {
+                        Ok(saved) => saved,
+                        Err(e) => {
+                            eprintln!("Failed to parse TOML: {}", e);
+                            Theme::default()
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Failed to read file: {}", e);
+                        Theme::default()
+                    }
+                }
+            } else {
+                Theme::default()
+            }
+        } else {
+            Theme::default()
+        }
+    }
 }
