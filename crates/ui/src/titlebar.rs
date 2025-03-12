364c8cf9679a6154685e8ccef8d8e078aabff8dd
Anant Narayan <anantnrg@gmail.com>
2025-03-12 14:45:03 +0530

    ui shit

--- crates/ui/src/titlebar.rs
+++ crates/ui/src/titlebar.rs
@@ -25,7 +25,7 @@ impl Render for Titlebar {
                     .flex()
                     .w_full()
                     .h_full()
-                    .pl(px(6.0))
+                    .pl(px(12.0))
                     .items_center()
                     .justify_start()
                     .child(
@@ -38,30 +38,6 @@ impl Render for Titlebar {
                             .rounded_md()
                             .child(
                                 Icon::new(Icons::Settings)
-                                    .size(18.0)
-                                    .color(theme.titlebar.icon.into())
-                                    .hover(theme.titlebar.hover.into()),
-                            )
-                            .on_mouse_down(MouseButton::Left, {
-                                move |_, _, cx| {
-                                    let layout = cx.global_mut::<Layout>().clone();
-                                    layout.left_sidebar.update(cx, |this, cx| {
-                                        this.should_show = !this.should_show.clone();
-                                        cx.notify();
-                                    });
-                                }
-                            }),
-                    )
-                    .child(
-                        div()
-                            .w_6()
-                            .h_6()
-                            .flex()
-                            .items_center()
-                            .justify_center()
-                            .rounded_md()
-                            .child(
-                                Icon::new(Icons::LeftSidebar)
                                     .size(18.0)
                                     .color(theme.titlebar.icon.into())
                                     .hover(theme.titlebar.hover.into()),
@@ -154,6 +130,30 @@ impl Render for Titlebar {
                     .justify_end()
                     .w_full()
                     .h_full()
+                    .child(
+                        div()
+                            .w_6()
+                            .h_6()
+                            .flex()
+                            .items_center()
+                            .justify_center()
+                            .rounded_md()
+                            .child(
+                                Icon::new(Icons::LeftSidebar)
+                                    .size(18.0)
+                                    .color(theme.titlebar.icon.into())
+                                    .hover(theme.titlebar.hover.into()),
+                            )
+                            .on_mouse_down(MouseButton::Left, {
+                                move |_, _, cx| {
+                                    let layout = cx.global_mut::<Layout>().clone();
+                                    layout.left_sidebar.update(cx, |this, cx| {
+                                        this.should_show = !this.should_show.clone();
+                                        cx.notify();
+                                    });
+                                }
+                            }),
+                    )
                     .child(
                         div()
                             .w_6()
