c0474fc16640095aa65b2f2a18b17e5d5d2e16b9
Anant Narayan <anantnrg@gmail.com>
2026-01-05 23:38:23 +0530

    Starting on UI

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,9 @@ use std::{thread, time::Duration};
 
 use crate::audio::engine::AudioEngine;
 use crate::controller::player::{AudioCommand, AudioEvent, Controller, PlayerState};
+use crate::ui::wiremann::Wiremann;
+use gpui::*;
+use gpui_component::{button::*, *};
 
 pub fn run() {
     let (audio_tx, audio_rx) = unbounded::<AudioCommand>();
@@ -14,6 +17,22 @@ pub fn run() {
     });
 
     let controller = Controller::new(audio_tx, event_rx, PlayerState::default());
+
+    let app = Application::new();
+
+    app.run(move |cx| {
+        gpui_component::init(cx);
+
+        cx.spawn(async move |cx| {
+            cx.open_window(WindowOptions::default(), |window, cx| {
+                let view = cx.new(|_| Wiremann);
+                cx.new(|cx| Root::new(view, window, cx))
+            })?;
+
+            Ok::<_, anyhow::Error>(())
+        })
+        .detach();
+    });
 
     loop {
         std::thread::park();
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod app;
 pub mod audio;
 pub mod controller;
+pub mod ui;
 
 fn main() {
     app::run();
