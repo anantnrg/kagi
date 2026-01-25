d4d9a9a9d530f70f2b1fcf50a852d25ddb8d1cf1
Anant Narayan <anantnrg@gmail.com>
2026-01-25 07:35:52 +0530

    having a fracture sucks. almost healed tho

--- src/ui/titlebar.rs
+++ src/ui/titlebar.rs
@@ -9,16 +9,14 @@ pub struct Titlebar;
 impl Render for Titlebar {
     fn render(&mut self, win: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         let theme = cx.global::<Theme>();
-        div().bg(theme.bg).size_full().child(
-            div()
-                .id("titlebar")
-                .h_10()
-                .w_full()
-                .border_b_1()
-                .border_color(theme.border)
-                .bg(theme.panel)
-                .window_control_area(WindowControlArea::Drag),
-        )
+        div()
+            .id("titlebar")
+            .h_10()
+            .w_full()
+            .border_b_1()
+            .border_color(theme.border)
+            .bg(theme.panel)
+            .window_control_area(WindowControlArea::Drag)
     }
 }
 
